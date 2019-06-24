f06a4332c5617b74ee7c7ba0010d4f8f69dc016f
MoonKuma <1244074267@qq.com>
2019-06-24 18:47:24 +0800

    Collinear

---
+++ collinear/FastCollinearPoints.java
@@ -0,0 +1,24 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class FastCollinearPoints {
+
+    public FastCollinearPoints(Point[] points) {
+        // finds all line segments containing 4 points
+    }
+    public int numberOfSegments() {
+        // the number of line segments
+        return 0;
+    }
+    public LineSegment[] segments() {
+        // the line segments
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
